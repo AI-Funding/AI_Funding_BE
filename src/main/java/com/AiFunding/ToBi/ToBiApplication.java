6cb3b25288e618f458a6051547d9935a3170ee6e
kwYoohae <gocksdl7979@gmail.com>
2021-11-28 19:22:25 +0900

    first commit

---
+++ src/main/java/com/AiFunding/ToBi/ToBiApplication.java
@@ -0,0 +1,13 @@
+package com.AiFunding.ToBi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToBiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToBiApplication.class, args);
+	}
+
+}
