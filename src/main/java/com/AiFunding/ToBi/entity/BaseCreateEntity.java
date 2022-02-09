0fe5ddd9beac338d58b412b73cdacdcd6f014461
Yoo Haechan <74089271+kwYoohae@users.noreply.github.com>
2022-02-09 21:21:38 +0900

    [추가] Timestamp 추가 (#28)

--- src/main/java/com/AiFunding/ToBi/entity/BaseCreateEntity.java
+++ src/main/java/com/AiFunding/ToBi/entity/BaseCreateEntity.java
@@ -1,13 +1,12 @@
 package com.AiFunding.ToBi.entity;
 
 import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Column;
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
@@ -16,7 +15,7 @@ import java.time.LocalDateTime;
 public abstract class BaseCreateEntity {
 
     @CreatedDate
-    @Column(name = "create_at")
+    @Column(name = "create_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private LocalDateTime createAt;
 
 }
