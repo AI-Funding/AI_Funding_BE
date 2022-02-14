fd4631f952d0a9a4238e551e6c52257116eaf330
kwYoohae <gocksdl7979@gmail.com>
2022-02-15 00:43:29 +0900

    [수정] profit 과 todayIncome 구하는 식 변경

--- src/main/java/com/AiFunding/ToBi/service/home/HomeService.java
+++ src/main/java/com/AiFunding/ToBi/service/home/HomeService.java
@@ -6,16 +6,13 @@ import com.AiFunding.ToBi.dto.Home.UserResponseDto;
 import com.AiFunding.ToBi.entity.AccountEntity;
 import com.AiFunding.ToBi.entity.AccountStockDetailEntity;
 import com.AiFunding.ToBi.entity.CustomerInformationEntity;
-import com.AiFunding.ToBi.entity.StockEntity;
 import com.AiFunding.ToBi.mapper.CustomerInformationRepository;
-import com.AiFunding.ToBi.mapper.StockRepository;
 import com.AiFunding.ToBi.service.comparator.AccountComparator;
 import com.AiFunding.ToBi.service.comparator.StockComparator;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -31,6 +28,7 @@ public class HomeService {
         this.customerInformationRepository = customerInformationRepository;
     }
 
+
     public UserResponseDto findUserInfo(final Long id, final String loginType){
         CustomerInformationEntity customer = customerInformationRepository.findByIdAndLoginType(id,loginType);
 
@@ -45,10 +43,10 @@ public class HomeService {
             Long stockBalance = account.getBalance(); // 현재 보유하고 있는 주식의 총 금액
 
             for(AccountStockDetailEntity stock : account.getAccountStocks()){ // 현재 보유하고 있는 주식의 총 금액을 더해주는 과정
-                stockBalance += (long) stock.getStockAmount() * stock.getAveragePrice();
+                stockBalance += (long) stock.getStockAmount() * stock.getStock().getNowPrice();
             }
-            Double todayIncome = (double)account.getTodayTotalBalance() / (double)account.getYesterdayTotalBalance() - 1.0;
-            Double totalIncome = (double) stockBalance / (double) INITIAL_PRICE - 1.0;
+            Double todayIncome = (double)(account.getTodayTotalBalance()-account.getYesterdayTotalBalance()) / account.getYesterdayTotalBalance() * 100;
+            Double totalIncome = (double)(stockBalance - INITIAL_PRICE) /INITIAL_PRICE * 100;
 
             accountData.add(new AccountListResponseDto(account.getNickname(), stockBalance,account.getCreateAt()
             ,Math.floor(todayIncome*100) / 100.0
@@ -69,16 +67,15 @@ public class HomeService {
 
         // 계좌가 가지고 있는 주식의 금액 보유량 구하기 -> 계좌에서 차지하는 비율을 구하기 위함
         for(AccountStockDetailEntity stocks : account.getAccountStocks()){
-            balance += (long) stocks.getAveragePrice() * stocks.getStockAmount();
+            balance += (long) stocks.getStock().getNowPrice() * stocks.getStockAmount();
         }
 
 
         for(AccountStockDetailEntity stocks : account.getAccountStocks()){
             //TODO: 해당 주식 id가 없을 경우 발생하는 오류 Exception 추가할 것
 
-            Double profitAndLoss = (stocks.getAveragePrice()*stocks.getStockAmount()*(1 +PURCHASE_FEE + PUBLIC_TAX)) / stocks.getStockAmount(); // 손익단가
-            Double profit = (profitAndLoss - stocks.getStock().getNowPrice()) / profitAndLoss * 100; // 수익률
-            Double accountPercent = (double)stocks.getStockAmount() * (double)stocks.getAveragePrice() / (double)balance * 100.0;
+            Double accountPercent = (double)stocks.getStockAmount() * (double)stocks.getStock().getNowPrice() / (double)balance * 100.0;
+            Double profit = (double)(stocks.getStock().getNowPrice() - stocks.getAveragePrice()) / stocks.getAveragePrice() * 100;
 
             stockList.add(new StockListResponseDto(
                     stocks.getStock().getItemName(),stocks.getStock().getNowPrice(),
@@ -88,4 +85,5 @@ public class HomeService {
         Collections.sort(stockList,new StockComparator()); // 점유율에 대해서 내림차순으로 정렬
         return stockList;
     }
+
 }
