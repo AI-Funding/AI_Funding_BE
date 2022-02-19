e42988a64156bd3cfd334bba5e7b2a2e28a02f92
JinSeoHan <87526189+JinSeoHan@users.noreply.github.com>
2022-02-19 22:10:30 +0900

    [수정] dto 이름 변경으로 인한 주석

--- src/test/java/StuckInfoDtoTest.java
+++ src/test/java/StuckInfoDtoTest.java
@@ -1,29 +1,7 @@
-import com.AiFunding.ToBi.dto.ai.page.StuckInfoResponseDto;
+import com.AiFunding.ToBi.dto.ai.page.StockInfoResponseDto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class StuckInfoDtoTest {
-    @Test
-    public void rombokTest(){
-        String stuckName = "한서진";
-        Long currentprice = Long.valueOf(10000);
-        Long priceFluc = Long.valueOf(2000);
-        Double rateFluc = 2.73;
-        String fluctuation = "UP";
 
-
-
-        StuckInfoResponseDto dto = new StuckInfoResponseDto(
-                stuckName,
-                currentprice,
-                priceFluc,
-                rateFluc,
-                fluctuation);
-
-        Assertions.assertThat(dto.getStuckName()).isEqualTo(stuckName);
-        Assertions.assertThat(dto.getCurrentPrice()).isEqualTo(currentprice);
-        Assertions.assertThat(dto.getPriceFluc()).isEqualTo(priceFluc);
-        Assertions.assertThat(dto.getRateFluc()).isEqualTo(rateFluc);
-        Assertions.assertThat(dto.getFluctuation()).isEqualTo(fluctuation);
-    }
 }
