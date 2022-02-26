ce6e4265e3182e1e6378b4ccd206fbb6bf517ede
jiwon <83341978+jiione@users.noreply.github.com>
2022-02-26 17:54:10 +0900

    .

--- src/main/java/com/AiFunding/ToBi/entity/CustomerInformationEntity.java
+++ src/main/java/com/AiFunding/ToBi/entity/CustomerInformationEntity.java
@@ -17,7 +17,6 @@ import java.util.List;
 @ToString // ToString을 오버라이딩합니다.
 @Getter // Getter를 사용합니다.
 @Table(name = "CUSTOMER_INFO")  // CUSTOMER_INFO라는 테이블에 매핑을 합니다.
-
 public class CustomerInformationEntity extends BaseCreateModifiedEntity {
 
     @Id // PK 값을 설정합니다.
