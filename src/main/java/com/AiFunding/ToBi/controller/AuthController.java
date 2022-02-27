9ddd7eb42a86c73c216efb9b38b6708897eca51e
jiwon <83341978+jiione@users.noreply.github.com>
2022-02-27 14:46:37 +0900

    refresh Token 반환

--- src/main/java/com/AiFunding/ToBi/controller/AuthController.java
+++ src/main/java/com/AiFunding/ToBi/controller/AuthController.java
@@ -1,8 +1,8 @@
 package com.AiFunding.ToBi.controller;
 
-import com.AiFunding.ToBi.service.OAuthService;
+import com.AiFunding.ToBi.dto.auth.TokenDto;
+import com.AiFunding.ToBi.service.oauth.OAuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,20 +15,28 @@ import java.util.HashMap;
 @RequiredArgsConstructor
 public class AuthController {
 
-    @Autowired
-    private OAuthService oAuthService;
+    private final OAuthService oAuthService;
 
     @RequestMapping(value = "/oauth2/code/kakao")
     public String kakaoLogin(@RequestParam("code") String code, HttpSession session){
-        String accessToken = oAuthService.getAccessToken(code);
-        HashMap<String, Object> userInfo = oAuthService.getUserInfo(accessToken);
-        System.out.println("controller access_token : " + accessToken);
+        TokenDto token = oAuthService.getAccessToken(code); // accessToken 값 가져오기
+        HashMap<String, String> userInfo = oAuthService.getUserInfo(token.getAccessToken());
+        System.out.println("controller access_token : " + token.getAccessToken());
         System.out.println("login Controller : " + userInfo);
+
+        userInfo.put("accessToken",token.getAccessToken()); // accessToken HashMap에 저장
+        userInfo.put("refreshToken",token.getRefreshToken()); // refreshToken HashMap에 저장
 
         //    클라이언트의 이메일이 존재할 때 세션에 해당 이메일과 토큰 등록
         if (userInfo.get("email") != null) {
             session.setAttribute("userId", userInfo.get("email"));
-            session.setAttribute("access_Token", accessToken);
+            session.setAttribute("access_Token", token.getAccessToken());
+        }
+
+        try{
+            oAuthService.saveUserInfo(userInfo,"kakao");
+        }catch (Exception e){
+            e.printStackTrace();
         }
 
         return "index";
