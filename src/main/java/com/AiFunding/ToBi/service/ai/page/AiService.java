3e2e07587b28291b253dbfb7fb1e952d337096e8
JinSeoHan <87526189+JinSeoHan@users.noreply.github.com>
2022-03-07 15:41:26 +0900

    [수정] static final 변수명수정 / 사용하지 않는 코드 삭제 / yesterdayStockPrice 코드수정 / 어제주식가격대비 상승가격 코드수정

--- src/main/java/com/AiFunding/ToBi/service/ai/page/AiService.java
+++ src/main/java/com/AiFunding/ToBi/service/ai/page/AiService.java
@@ -23,10 +23,8 @@ public class AiService {
     private final StockPriceByDayRepository stockPriceByDayRepository;//주가변동내용 날짜별 주식가격 Repository
     private final AccountStockDetailRepository accountStockDetailRepository;//계좌주식잔고 주식의세부정보 Repository
 
-    public static final LocalDateTime todayStartTime = LocalDateTime.of(LocalDate.now(), LocalTime.of(0, 0, 0));
-    public static final LocalDateTime todayEndTime = LocalDateTime.of(LocalDate.now(), LocalTime.of(23, 59, 59));
-    public static final LocalDateTime yesterdayStartTime = LocalDateTime.of(LocalDate.now().minusDays(1), LocalTime.of(0, 0, 0));
-    public static final LocalDateTime yesterdayEndTime = LocalDateTime.of(LocalDate.now().minusDays(1), LocalTime.of(23, 59, 59));
+    public static final LocalDateTime TODAY_START_TIME = LocalDateTime.of(LocalDate.now(), LocalTime.of(0, 0, 0));
+    public static final LocalDateTime TODAY_END_TIME = LocalDateTime.of(LocalDate.now(), LocalTime.of(23, 59, 59));
 
     //Repository 초기화
     public AiService(CustomerInformationRepository customerInformationRepository, StockPriceByDayRepository stockPriceByDayRepository, AccountStockDetailRepository accountStockDetailRepository) {
@@ -56,20 +54,20 @@ public class AiService {
     //주식정보들을 Dto로 담아서 반환
     public List<StockInfoResponseDto> getStockDtoList(AccountEntity account) {
         List<StockInfoResponseDto> stockDtoList = new ArrayList<>();
-        //List<StockPriceByDayEntity> stockPriceByDay = stockPriceByDayRepository.findByStockEntityAndCreateAtBetween()
 
         //계좌Entity에 있는 모든 주식들을 하나씩 가져온다
         for (AccountStockDetailEntity stock : account.getAccountStocks()) {
-            List<StockPriceByDayEntity> stockPriceByDayEntities = stockPriceByDayRepository.findByStockAndCreateAtBetween(stock.getStock(), yesterdayStartTime, yesterdayEndTime);
+            List<StockPriceByDayEntity> stockPriceByDayEntities = stockPriceByDayRepository.findByStockAndCreateAtBetween(stock.getStock(), TODAY_START_TIME, TODAY_END_TIME);//오늘 주식가격변동내용 Entity
             Integer yesterdayStockPrice = stockPriceByDayEntities.get(stockPriceByDayEntities.size()-1).getEndPrice();//어제주식가격
             Integer todayStockPrice = stock.getStock().getNowPrice();//오늘 주식가격
-            Integer stockPriceChange = todayStockPrice * stock.getStockAmount() - yesterdayStockPrice * stock.getStockAmount();//어제주식가격 대비 상승가격
+            Integer stockPriceChange = todayStockPrice - yesterdayStockPrice;//어제주식가격 대비 상승가격
             double stockRateChange = ((double)(todayStockPrice - yesterdayStockPrice) / yesterdayStockPrice) * 100;
 
+            //상승 하락 변동률 계산
             int stockChange = 0;
             if (stockRateChange > 0)
                 stockChange = 1;
-            else
+            else if(stockRateChange < 0)
                 stockChange = -1;
 
             stockDtoList.add(new StockInfoResponseDto(stock.getStock().getItemName(), new Long(todayStockPrice), new Long(stockPriceChange), stockRateChange, stockChange ,getStockDetailDtoList(stock)));
@@ -80,14 +78,14 @@ public class AiService {
     //주식 세부정보를 Dto에 담아서 반환
     public List<StockDetailResponseDto> getStockDetailDtoList(AccountStockDetailEntity stocks) {
         List<StockDetailResponseDto> stockDetailDtoList = new ArrayList<>();
-        for (AccountStockDetailEntity stockDetail : stocks.getStock().getAccountStockDetailEntities()) {
-            List<StockPriceByDayEntity> stockPriceByDayEntities = stockDetail.getStock().getStockPriceByDayEntities();
-            StockPriceByDayEntity stockPriceByDay = stockPriceByDayEntities.get(stockPriceByDayEntities.size()-1);
+
+        for (StockPriceByDayEntity stockPriceByDay : stocks.getStock().getStockPriceByDayEntities()) {
             stockDetailDtoList.add(new StockDetailResponseDto(stockPriceByDay.getEndPrice(), stockPriceByDay.getCreateAt()));
         }
         return stockDetailDtoList;
     }
 
 }
+
 
 
