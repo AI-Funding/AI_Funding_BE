25e74d27f416966aaae930a0cf4b025116c4ba40
kwYoohae <gocksdl7979@gmail.com>
2022-03-17 21:55:12 +0900

    [수정] Entity 잘못설정된거 수정

--- src/main/java/com/AiFunding/ToBi/mapper/TradingDetailRepository.java
+++ src/main/java/com/AiFunding/ToBi/mapper/TradingDetailRepository.java
@@ -1,7 +1,8 @@
 package com.AiFunding.ToBi.mapper;
 
+import com.AiFunding.ToBi.entity.TradeSignalEntity;
 import com.AiFunding.ToBi.entity.TradingDetailEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TradingDetailRepository extends JpaRepository<TradingDetailEntity,Long> {
+public interface TradingDetailRepository extends JpaRepository<TradeSignalEntity,Long> {
 }
