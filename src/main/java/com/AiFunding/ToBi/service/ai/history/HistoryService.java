aeaffe083803d41abbd355f109a48fe7f31fbde1
JinSeoHan <87526189+JinSeoHan@users.noreply.github.com>
2022-03-17 22:47:41 +0900

    [수정] Service통합수정

--- src/main/java/com/AiFunding/ToBi/service/ai/history/HistoryService.java
+++ src/main/java/com/AiFunding/ToBi/service/ai/history/HistoryService.java
@@ -1,7 +1,8 @@
 package com.AiFunding.ToBi.service.ai.history;
 
-import com.AiFunding.ToBi.dto.ai.History.HistoryListResponseDto;
-import com.AiFunding.ToBi.dto.ai.History.HistoryResponseDto;
+import com.AiFunding.ToBi.dto.ai.History.AccountTradeHistoryResponseDto;
+import com.AiFunding.ToBi.dto.ai.History.TradeHistoryResponseDto;
+import com.AiFunding.ToBi.dto.ai.History.TradeHistoryListResponseDto;
 import com.AiFunding.ToBi.entity.AccountEntity;
 import com.AiFunding.ToBi.entity.CustomerInformationEntity;
 import com.AiFunding.ToBi.entity.TradingDetailEntity;
@@ -9,6 +10,8 @@ import com.AiFunding.ToBi.mapper.CustomerInformationRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -21,32 +24,38 @@ public class HistoryService {
     }
 
     // User 정보를 찾고 거래 내역 DTO 를 반환
-    public HistoryResponseDto findUserHistory(final Long id, final String loginType) {
+    public AccountTradeHistoryResponseDto findUserHistory(final Long id, final String loginType) {
         CustomerInformationEntity customerInfo = customerInformationRepository.findByIdAndLoginType(id, loginType);
 
-        return new HistoryResponseDto(getHistoryDtoList(customerInfo.getAccounts()));
+        return new AccountTradeHistoryResponseDto(getAccountHistoryDtoList(customerInfo.getAccounts()));
     }
 
     // 거래내역 관련 데이터 리스트 반환
-    public List<HistoryListResponseDto> getHistoryDtoList(List<AccountEntity> accounts){
-        List<HistoryListResponseDto> historyDtoList = new ArrayList<>();
+    public List<TradeHistoryListResponseDto> getAccountHistoryDtoList(List<AccountEntity> accounts){
+        List<TradeHistoryListResponseDto> tradeHistoryListResponseDtos = new ArrayList<>();
 
         for(AccountEntity account : accounts){
-
-            for(TradingDetailEntity trading : account.getTradingEntities()){
-                String tradeType = trading.getTradingType();                //거래 종류
-                Integer tradingAmount = trading.getTradingAmount();         //거래 수량
-                Long tradingPrice = trading.getTradingPrice();              //매수/도 가
-                Long totalPrice = trading.getTradingPrice();                //총 거래 금액
-                Long currentPrice = totalPrice / (long)tradingAmount;       //단가, 1주당 가격
-
-                historyDtoList.add(new HistoryListResponseDto(trading.getStock().getItemName(), trading.getCreateAt(),
-                        totalPrice, tradeType, tradingAmount, currentPrice, tradingPrice));
-            }
-
+            tradeHistoryListResponseDtos.add(new TradeHistoryListResponseDto(account.getNickname(), getHistoryDtoList(account.getTradingEntities())));  //new TradeHistoryListResponseDto(account.getNickname(), getHistoryDtoList(account.getTradingEntities()))
         }
         
-        Collections.sort(historyDtoList, Comparator.comparing(HistoryListResponseDto::getTradeDate));   //시간 순 정렬
+        return tradeHistoryListResponseDtos;
+    }
+
+    public List<TradeHistoryResponseDto> getHistoryDtoList(List<TradingDetailEntity> tradings){
+        List<TradeHistoryResponseDto> historyDtoList = new ArrayList<>();
+
+        for(TradingDetailEntity trading : tradings){
+            String tradeType = trading.getTradingType();                //거래 종류
+            Integer tradingAmount = trading.getTradingAmount();         //거래 수량
+            Long tradingPrice = trading.getTradingPrice();              //매수/도 가
+            Long totalPrice = trading.getTradingPrice();                //총 거래 금액
+            Long currentPrice = totalPrice / (long)tradingAmount;       //단가, 1주당 가격
+
+            historyDtoList.add(new TradeHistoryResponseDto(trading.getStock().getItemName(), trading.getCreateAt(),
+                    totalPrice, tradeType, tradingAmount, currentPrice, tradingPrice));
+        }
+
+        Collections.sort(historyDtoList, Comparator.comparing(TradeHistoryResponseDto::getTradeDate));   //시간 순 정렬
         return historyDtoList;
     }
 }
