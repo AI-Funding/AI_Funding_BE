73fd636a7a8399b893e53c6c8da70c757a03227b
jiwon <83341978+jiione@users.noreply.github.com>
2022-03-20 14:27:10 +0900

    .

--- src/main/java/com/AiFunding/ToBi/mapper/CustomerInformationRepository.java
+++ src/main/java/com/AiFunding/ToBi/mapper/CustomerInformationRepository.java
@@ -4,10 +4,13 @@ import com.AiFunding.ToBi.entity.CustomerInformationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface CustomerInformationRepository extends JpaRepository<CustomerInformationEntity,Long> {
     // user_sequence와 login_type을 통한 유저 정보 찾기
 //    @Query(value = "SELECT * FROM CUST_INFO CUST_INFO WHERE user_sequence=?0 AND login_type=?1", nativeQuery = true)
     CustomerInformationEntity findByIdAndLoginType(Long id, String loginType);
 
-    CustomerInformationEntity findByUserIdAndLoginType(String userId, String loginType);
+
+    Optional<CustomerInformationEntity> findByUserIdAndLoginType(String userId, String loginType);
 }
