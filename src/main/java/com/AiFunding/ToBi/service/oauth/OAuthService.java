a34af2dfa3a4b0cb1f9b592b70cd9eac1232dead
kwYoohae <gocksdl7979@gmail.com>
2022-03-20 14:51:47 +0900

    [수정] 충돌된 오류 모두 수정

--- src/main/java/com/AiFunding/ToBi/service/oauth/OAuthService.java
+++ src/main/java/com/AiFunding/ToBi/service/oauth/OAuthService.java
@@ -1,17 +1,17 @@
 package com.AiFunding.ToBi.service.oauth;
 
-
+import com.AiFunding.ToBi.dto.auth.LoginRequestDto;
 import com.AiFunding.ToBi.dto.auth.SocialLoginType;
+import com.AiFunding.ToBi.dto.auth.TokenDto;
 import com.AiFunding.ToBi.entity.CustomerInformationEntity;
 import com.AiFunding.ToBi.exception.AlreadyExistUser;
 import com.AiFunding.ToBi.mapper.CustomerInformationRepository;
-import com.AiFunding.ToBi.service.oauth.social.GoogleOauth;
-import com.AiFunding.ToBi.service.oauth.social.KakaoOauth;
 import com.AiFunding.ToBi.service.oauth.social.SocialOauth;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.transaction.Transactional;
 import java.io.*;
 import java.util.HashMap;
 import java.util.List;
@@ -21,12 +21,11 @@ import java.util.List;
 public class OAuthService {
 
     private final CustomerInformationRepository customerInformationRepository;
-
     private final JwtTokenUtils jwtTokenUtils;
     private final HttpServletResponse response;
     private final List<SocialOauth> socialOauthList;
 
-    public void request(SocialLoginType socialLoginType){
+    public void request(SocialLoginType socialLoginType) {
         SocialOauth socialOauth = this.findSocialOauthByType(socialLoginType);
         String redirectURL = socialOauth.getOauthRedirectURL();
         try {
@@ -41,20 +40,20 @@ public class OAuthService {
         return socialOauth.requestAccessToken(code);
     }
 
-    public String requestUserInfo(SocialLoginType socialLoginType,String accessToken) {
+    public String requestUserInfo(SocialLoginType socialLoginType, String accessToken) {
         SocialOauth socialOauth = this.findSocialOauthByType(socialLoginType);
         return socialOauth.getUserId(accessToken);
     }
 
-    public boolean isExistsUser(String loginType, String userId){
-        if(customerInformationRepository.findByUserIdAndLoginType(userId,loginType) == null){
+    public boolean isExistsUser(String loginType, String userId) {
+        if (customerInformationRepository.findByUserIdAndLoginType(userId, loginType) == null) {
             return false;
         }
         return true;
-
+    }
 
     public void saveUserInfo(HashMap<String, String> userInfo, String loginType) throws AlreadyExistUser {
-        if(customerInformationRepository.findByUserIdAndLoginType(userInfo.get("id"),loginType) != null){
+        if (customerInformationRepository.findByUserIdAndLoginType(userInfo.get("id"), loginType) != null) {
             throw new AlreadyExistUser();
         }
         customerInformationRepository.save(
@@ -72,4 +71,46 @@ public class OAuthService {
                 .orElseThrow(() -> new IllegalArgumentException("알 수 없는 SocialLoginType 입니다."));
     }
 
+    @Transactional
+    public TokenDto signIn(String userId, String loginType) {
+        CustomerInformationEntity customerInformationEntity =
+                customerInformationRepository
+                        .findByUserIdAndLoginType(userId,loginType)
+                        .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
+
+        String accessToken = "";
+        String refreshToken = customerInformationEntity.getRefreshToken();
+
+        if(jwtTokenUtils.isValidRefreshToken(refreshToken)){
+            accessToken = jwtTokenUtils.createJwtToken(customerInformationEntity);
+            return TokenDto.builder()
+                    .accessToken(accessToken)
+                    .refreshToken(refreshToken)
+                    .build();
+        }
+        else{
+            refreshToken = jwtTokenUtils.createRefreshToken();
+            customerInformationEntity.builder()
+                    .refreshToken(refreshToken)
+                    .build();
+        }
+
+        return TokenDto.builder().accessToken(accessToken).refreshToken(refreshToken).build();
+    }
+
+    @Transactional
+    public TokenDto signUp(LoginRequestDto loginRequestDto) {
+        String refreshToken = jwtTokenUtils.createRefreshToken();
+        CustomerInformationEntity customerInformationEntity =
+                customerInformationRepository.save(
+                        CustomerInformationEntity.builder()
+                                .nickname(loginRequestDto.getNickname())
+                                .userId(loginRequestDto.getUID())
+                                .email(loginRequestDto.getEmail())
+                                .loginType(loginRequestDto.getLoginType())
+                                .refreshToken(refreshToken)
+                                .build());
+        String accessToken = jwtTokenUtils.createJwtToken(customerInformationEntity);
+        return TokenDto.builder().accessToken(accessToken).refreshToken(refreshToken).build();
+    }
 }
