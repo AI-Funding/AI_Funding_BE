63249151fd90edc20e1d020eb72d2460b6bb8a42
Yoo Haechan <74089271+kwYoohae@users.noreply.github.com>
2022-03-20 14:53:41 +0900

    [수정] 충돌된 오류 모두 수정 (#57)

--- src/main/java/com/AiFunding/ToBi/service/ai/history/HistoryService.java
+++ src/main/java/com/AiFunding/ToBi/service/ai/history/HistoryService.java
@@ -9,6 +9,8 @@ import com.AiFunding.ToBi.mapper.CustomerInformationRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 @Service
