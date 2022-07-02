811e6f03f5363678b1f08d1ec6af49820cfffc73
kwYoohae <gocksdl7979@gmail.com>
2022-07-02 16:13:44 +0900

    [추가] find 메서드 추가

--- src/main/java/com/AiFunding/ToBi/mapper/AccountStockDetailRepository.java
+++ src/main/java/com/AiFunding/ToBi/mapper/AccountStockDetailRepository.java
@@ -1,7 +1,9 @@
 package com.AiFunding.ToBi.mapper;
 
 import com.AiFunding.ToBi.entity.AccountStockDetailEntity;
+import com.AiFunding.ToBi.entity.StockEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountStockDetailRepository extends JpaRepository<AccountStockDetailEntity, Long> {
+    AccountStockDetailEntity findByStockAndId(StockEntity stockEntity, Long id);
 }
--- src/main/java/com/AiFunding/ToBi/mapper/TradingDetailRepository.java
+++ src/main/java/com/AiFunding/ToBi/mapper/TradingDetailRepository.java
@@ -1,7 +1,13 @@
 package com.AiFunding.ToBi.mapper;
 
+import com.AiFunding.ToBi.entity.AccountEntity;
 import com.AiFunding.ToBi.entity.TradingDetailEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface TradingDetailRepository extends JpaRepository<TradingDetailEntity,Long> {
+
+    List<TradingDetailEntity> findByCreateAtBetweenAndAccount(LocalDateTime startTime, LocalDateTime endTime, AccountEntity account);
 }
