33e9667c6f41df42bf37a86e9b9b6756265ef7ac
kwYoohae <gocksdl7979@gmail.com>
2022-07-07 23:14:42 +0900

    [수정] 오류사항 수정

--- src/main/java/com/AiFunding/ToBi/mapper/AccountDetailRepository.java
+++ src/main/java/com/AiFunding/ToBi/mapper/AccountDetailRepository.java
@@ -1,7 +1,10 @@
 package com.AiFunding.ToBi.mapper;
 
 import com.AiFunding.ToBi.entity.AccountDetailEntity;
+import com.AiFunding.ToBi.entity.AccountStockDetailEntity;
+import com.AiFunding.ToBi.entity.StockEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountDetailRepository extends JpaRepository<AccountDetailEntity,Long> {
+
 }
--- src/main/java/com/AiFunding/ToBi/mapper/StockRepository.java
+++ src/main/java/com/AiFunding/ToBi/mapper/StockRepository.java
@@ -3,5 +3,10 @@ package com.AiFunding.ToBi.mapper;
 import com.AiFunding.ToBi.entity.StockEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StockRepository extends JpaRepository<StockEntity,Long> {
+
+    Optional<StockEntity> findByItemName(String stockName);
+
 }
