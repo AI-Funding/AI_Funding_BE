ec38a997ed8870b64848f0a6b864339b48abfc95
HanSeoJin <yp6484@gmail.com>
2022-07-13 19:16:10 +0900

    [수정] 주석수정

--- src/main/java/com/AiFunding/ToBi/mapper/CustomerInformationRepository.java
+++ src/main/java/com/AiFunding/ToBi/mapper/CustomerInformationRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface CustomerInformationRepository extends JpaRepository<CustomerInformationEntity,Long> {
-    // user_sequence와 lo        gin_type을 통한 유저 정보 찾기
+    // user_sequence와 login_type을 통한 유저 정보 찾기
 //    @Query(value = "SELECT * FROM CUST_INFO CUST_INFO WHERE user_sequence=?0 AND login_type=?1", nativeQuery = true)
     Optional<CustomerInformationEntity> findByIdAndLoginType(Long id, String loginType);
 
