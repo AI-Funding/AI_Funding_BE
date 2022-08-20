4bfca2a09731a08811426d078a7fd68408513651
HanSeoJin <87526189+JinSeoHan@users.noreply.github.com>
2022-08-20 17:21:11 +0900

    Feature/#70커뮤니잡담탭구현 (#73)
    * [추가] 수익비교탭 DTO구현
    
    * [삭제] 불필요한 import삭제
    
    * [추가] Optional Null 처리 추가
    
    * [추가] 수익비교 Service구현
    
    * [추가] 수익비교 Controller구현
    
    * [추가] 커뮤니티 잡담 탭 DTO 구현
    
    * [수정] DTO 타입 및 파일 이름 수정
    
    * [추가] postType으로 board검색 기능 추가
    
    * [추가] 커뮤니티 잡담 탭 서비스 구현
    
    * [추가] 커뮤니티 잡담 탭 Controller구현
    
    * [추가] id와 loginType을 확인하기 위한 메서드 추가
    
    * [수정] 멤버 체크를 위한 필드 추가
    
    * [수정] CommunityChatResponseDto 필드  Wrapper타입 Boolean으로 수정
    
    * [수정] CommunityChatService id loginType 매개변수 추가
    
    * [수정] Controller id loginType 매개변수 추가

--- src/main/java/com/AiFunding/ToBi/mapper/BoardRepository.java
+++ src/main/java/com/AiFunding/ToBi/mapper/BoardRepository.java
@@ -3,5 +3,8 @@ package com.AiFunding.ToBi.mapper;
 import com.AiFunding.ToBi.entity.BoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
+    Optional<BoardEntity> findByPostType(String postType);
 }
